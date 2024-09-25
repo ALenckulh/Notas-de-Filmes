0f484ffeabd272fdb2158c3b02736c68c4a728b8
ALenckulh <127156754+ALenckulh@users.noreply.github.com>
2024-09-25 10:12:11 -0300

    cores padronizadas

---
+++ lib/themes/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const primary300 = Color(0xFF1C0B2B);
+  static const primary200 = Color(0xFF301C41);
+  static const primary100 = Color(0xFF413B6B);
+  static const primary = Color(0xFF5C65C0);
+  static const primary50 = Color(0xFF6F95FF);
+
+  static final hover = Colors.white.withOpacity(0.2);
+}
---
+++ lib/themes/custom_themes.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:notas/themes/colors.dart';
+
+class CustomThemes {
+  static const textStyle = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 16.0,
+  );
+
+  final defaultTheme = ThemeData(
+    scaffoldBackgroundColor: Colors.white70,
+    primaryColor: MyColors.primary,
+    primaryTextTheme: const TextTheme(
+      headlineLarge: TextStyle(
+        fontSize: 96,
+        fontWeight: FontWeight.w600,
+      ),
+      headlineMedium: TextStyle(
+        fontSize: 60,
+        fontWeight: FontWeight.w600,
+      ),
+      headlineSmall: TextStyle(
+        fontSize: 48,
+        fontWeight: FontWeight.w600,
+      ),
+      titleLarge: TextStyle(
+        fontSize: 34,
+        fontWeight: FontWeight.w500,
+      ),
+      titleMedium: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.w500,
+      ),
+      titleSmall: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w500,
+      ),
+      bodyLarge: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.normal,
+      ),
+      bodyMedium: TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w500,
+      ),
+      bodySmall: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+      ),
+      labelLarge: TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.w600,
+      ),
+      labelSmall: TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w600,
+        color: Colors.white,
+        letterSpacing: 1.0,
+      ),
+    ),
+    buttonTheme: const ButtonThemeData(
+      buttonColor: MyColors.primary,
+      textTheme: ButtonTextTheme.primary,
+    ),
+    inputDecorationTheme: const InputDecorationTheme(
+      border: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: MyColors.primary,
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: MyColors.primary,
+        ),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: MyColors.primary,
+        ),
+      ),
+    ),
+  );
+}
