0b0b51c285be1ad8c71174dc6719df14b109d597
Samara Cassie <samaracassie@outlook.com>
2024-10-09 08:29:09 -0300

    feat: route com ai e onTap

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:notas/data/avaliacoes.dart';
 import 'package:notas/pages/view_movie.dart';
 import 'package:notas/themes/custom_themes.dart';
-import 'package:notas/widget/button.dart';
 import 'package:notas/widget/header.dart';
 
 void main() {
@@ -19,15 +18,35 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => const MyHomePage(),
-        '/movie': (context) => const ViewMovie(),
+        '/movie': (context) => ViewMovie(ModalRoute.of(context)!.settings.arguments as String),
       },
       theme: CustomThemes().defaultTheme,
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Set<String> categorias;
+
+
+  @override
+  void initState() {
+    super.initState();
+    getCategorias(); // Chame a função para obter as categorias
+  }
+  void getCategorias() {
+    List<Map<String, dynamic>> avaliacoes = carregarAvaliacoes();
+    categorias = avaliacoes.map((avaliacao) => avaliacao['genero'].toString()).toSet();
+  }
+
+
 
   // Método para buscar filme por genero
   List<Map<String, dynamic>> buscarFilmesPorGenero(String genero) {
@@ -37,9 +56,10 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     List<Map<String, dynamic>> filmes = buscarFilmesPorGenero("Fantasia");
 
-    List imagens = filmes.map((filme) => filme['img'][0]).toList();
+    getCategorias();
 
     return Scaffold(
       appBar: const Header(),
@@ -51,23 +71,20 @@ class MyHomePage extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: <Widget>[
-                    Button(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/movie');
-                      },
-                      text: "Filme 1",
-                    ),
                     ConstrainedBox(
                       constraints: const BoxConstraints(maxHeight: 300),
                       child: CarouselView(
+                        onTap: (index) {
+                          Navigator.pushNamed(context, '/movie', arguments: '1');
+                          },
                         itemExtent: 250,
                         itemSnapping: true,
-                          children: imagens.map<Widget>((image) {
-                            return Image.asset(
-                              image, // Exibe a imagem de cada filme
-                              fit: BoxFit.cover,
-                            );
-                          }).toList(),// Convert to a list of widgets
+                        children: filmes.map<Widget>((filme) {
+                          return Image.asset(
+                            filme['img'][0], // Exibe a imagem de cada filme
+                            fit: BoxFit.cover,
+                          );
+                        }).toList(),// Convert to a list of widgets
                       ),
                     ), // CarouselView
                   ],
